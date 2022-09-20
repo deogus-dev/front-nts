b40a7f5537ba2bd3dfa2fc70cc94c6da017bd546
Deahyeon Jang <zdh8456@gmail.com>
2022-09-20 17:25:46 +0900

    axios interceptor 수정

--- src/axios.js
+++ src/axios.js
@@ -7,13 +7,7 @@ axios.interceptors.request.use(
 
     // 토큰 valid check를 위해 header 설정
     config.headers["Content-Type"] = "application/json; charset=utf-8";
-    config.headers["Authorization"] = "baerer test";
-
-    // 회원 id default set
-    if (config.params == null) {
-      config.params = {};
-    }
-    config.params.userId = "userid";
+    config.headers["Authorization"] = "baerer test"; //localstorage 또는 vuex에 저장된 사용자 access 토큰값
 
     return config;
   },
@@ -29,6 +23,10 @@ axios.interceptors.response.use(
   function (response) {
     // 응답 데이터를 가공
     // 토큰 만료 시 토큰 재발급 요청 후 재발급 된 access token으로 api 재요청
+
+    // response status 코드에 따른 후처리
+    // ex) 권한error 인 경우 재로그인 요청 alert와 함께 로그인페이지로 이동
+
     return response;
   },
   function (error) {
