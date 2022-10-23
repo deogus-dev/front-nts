bddaa9f08a1c54ffbef38acfbd386fd74ad59b02
daehyeon jang <deogus.dev@gmail.com>
2022-10-23 23:57:21 +0900

    퍼블작업 후 로그인 불가현상 수정

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,25 +3,60 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-6 text-black">
-          <div class="px-5 ms-xl-4" style="text-align:left;">
+          <div class="px-5 ms-xl-4" style="text-align: left">
             <span class="h1 fw-bold mb-0">안녕하세요.</span>
             <p>회원 서비스 이용 위해 로그인 해주세요.</p>
           </div>
-          <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-            <form style="width: 23rem;">
+          <div
+            class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5"
+          >
+            <form style="width: 23rem">
               <div class="form-outline mb-4">
-                <label class="form-label" for="email" style="text-align:left;">Email</label>
-                <input type="email" id="email" class="form-control form-control-lg" />
+                <label class="form-label" for="email" style="text-align: left"
+                  >Email</label
+                >
+                <input
+                  type="email"
+                  id="email"
+                  v-model="user"
+                  class="form-control form-control-lg"
+                />
               </div>
               <div class="form-outline mb-4">
-                <label class="form-label" for="password" style="text-align:left;">Password</label>
-                <input type="password" id="password" class="form-control form-control-lg" />
+                <label
+                  class="form-label"
+                  for="password"
+                  style="text-align: left"
+                  >Password</label
+                >
+                <input
+                  type="password"
+                  id="password"
+                  v-model="password"
+                  class="form-control form-control-lg"
+                />
               </div>
               <div class="pt-1 mb-4">
-                <button class="btn btn-secondary btn-lg btn-block mb-3" style="width: 100%;" type="button" @click="login">로그인</button>
-                <button class="btn btn-secondary btn-lg btn-block" style="width: 100%;" type="button" @click="signup">회원가입</button>
+                <button
+                  class="btn btn-secondary btn-lg btn-block mb-3"
+                  style="width: 100%"
+                  type="button"
+                  @click="login"
+                >
+                  로그인
+                </button>
+                <button
+                  class="btn btn-secondary btn-lg btn-block"
+                  style="width: 100%"
+                  type="button"
+                  @click="signup"
+                >
+                  회원가입
+                </button>
               </div>
-              <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">비밀번호 찾기</a></p>
+              <p class="small mb-5 pb-lg-2">
+                <a class="text-muted" href="#!">비밀번호 찾기</a>
+              </p>
             </form>
           </div>
         </div>
@@ -52,9 +87,10 @@ export default {
         });
         await this.$router.push("/");
       } catch (err) {
+        alert(err);
         this.loginError = true;
         this.error = true;
-        // throw new Error(err)
+        throw new Error(err);
       }
     },
     async signup() {
