17b940732be7a85eb969b8c0ba6c82242e19acc1
Deahyeon Jang <zdh8456@gmail.com>
2022-11-30 13:46:06 +0900

    index 퍼블 수정

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container h-100 index-container">
-    <h1 class="main-title">0918</h1>
-    <h5 class="sub-title">간편한<br />출근의 시작</h5>
+    <div class="title-area">
+      <h1 class="main-title mx-4 my-2">0918</h1>
+      <h5 class="sub-title mx-4 my-2">간편한<br />출근의 시작</h5>
+    </div>
     <div class="button-area">
       <router-link to="/login" class="btn w-100 loginBtn py-3 my-2"
         >로그인 후 이용하기</router-link
